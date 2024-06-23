2e425edd2bcf0c04a12f6973bada6bf4726f28bb
Husny Ahamed <husnyahamed@Husnys-MBP.home>
2024-06-23 17:58:00 +0200

    form and survey in single screen to be easy

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,13 @@ const ENDPOINT = "https://qx24vjuooi.execute-api.us-east-1.amazonaws.com/dev";
 
 function App() {
   const [name, setName] = useState<string>("");
+  const [password, setPassword] = useState("");
   const [selectedItems, setSelectedItems] = useState<string[]>([]);
-  const [isNameSubmitted, setIsNameSubmitted] = useState<boolean>(false);
 
   const handleCheckboxChange = (key: string) => {
     setSelectedItems((prev) =>
       prev.includes(key) ? prev.filter((item) => item !== key) : [...prev, key]
     );
-  };
-
-  const handleNameSubmit = () => {
-    setIsNameSubmitted(true);
   };
 
   const handleSubmit = async () => {
@@ -27,19 +23,24 @@ function App() {
 
     const data = {
       name,
+      password,
       selectedValues,
     };
 
     console.log(data);
 
     try {
-      await axios.post(ENDPOINT, data, {
+      const response = await axios.post(ENDPOINT, data, {
         headers: {
           "Content-Type": "application/json",
         },
       });
 
-      alert(`Success`);
+      if (response.data.statusCode === 200) {
+        alert(`Success`);
+      } else {
+        alert("Failed");
+      }
     } catch (error) {
       alert("Failed to submit data.");
     }
@@ -48,53 +49,39 @@ function App() {
   return (
     <div className="App">
       <h1>Personality Quiz</h1>
-      {!isNameSubmitted ? (
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-          }}
-        >
-          <div className="name-form">
-            <label htmlFor="name">Enter your name:</label>
+
+      <div className="name-form">
+        <label htmlFor="name">Enter your name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="password">Enter password:</label>
+        <input
+          type="text"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <>
+        {questions.map(({ key, value }) => (
+          <div key={key} className="question-item">
             <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              type="checkbox"
+              id={`question-${key}`}
+              checked={selectedItems.includes(key)}
+              onChange={() => handleCheckboxChange(key)}
             />
-            <button
-              type="button"
-              style={{ width: 200 }}
-              onClick={handleNameSubmit}
-              className="submit-button"
-            >
-              Start
-            </button>
+            <label htmlFor={`question-${key}`}>{value}</label>
           </div>
-        </form>
-      ) : (
-        <form>
-          {questions.map(({ key, value }) => (
-            <div key={key} className="question-item">
-              <input
-                type="checkbox"
-                id={`question-${key}`}
-                checked={selectedItems.includes(key)}
-                onChange={() => handleCheckboxChange(key)}
-              />
-              <label htmlFor={`question-${key}`}>{value}</label>
-            </div>
-          ))}
-          <button
-            type="button"
-            onClick={handleSubmit}
-            className="submit-button"
-          >
-            Submit Answers
-          </button>
-        </form>
-      )}
+        ))}
+        <button type="button" onClick={handleSubmit} className="submit-button">
+          Submit Answers
+        </button>
+      </>
     </div>
   );
 }
